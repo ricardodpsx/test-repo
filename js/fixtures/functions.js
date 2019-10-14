fffb902f8ff5bd01afdb7cd9b27819ed2e874026
ricardodps <ricardo.pacheco@thoughtworks.com>
2019-10-14 08:28:36 +0200

    support

--- js/fixtures/functions.js
+++ js/fixtures/functions.js
@@ -1,7 +1,3 @@
-function noArgs() {
-
-}
-
 function functionWith3Params(x, y, z) {
 
 }
@@ -9,16 +5,6 @@ function functionWith3Params(x, y, z) {
 function functionWith2Lines(p1, p2) {
   let p3 = 0
   return p1 * p2;   // The function returns the product of p1 and p2
-}
-
-
-
-var x = myFunction(4, 3);   // Function is called, return value will end up in x
-
-function myFunction2() {
-  var carName = "Volvo";
-  // code here CAN use carName
-  return carName
 }
 
 function functionWith2NestingLevels() {
@@ -30,4 +16,7 @@ function functionWith2NestingLevels() {
   }
   return carName
 }
+
+
+var res = functionWith3Params(1, 2, 3);   // Function is called, return value will end up in x
 
